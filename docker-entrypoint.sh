a054130319cb6be809c3cb3d03115bb975cd0dcd
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2021-03-08 17:26:11 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 if [ "$#" -eq 0 ]; then
   find tests  -type d -name '__pycache__' -print0 | xargs -0 rm -rf {}
   pyenv rehash
-  pyenv versions
+  for ver in `ls ~/.pyenv/versions`; do pyenv local $ver; python setup.py bdist_wheel -p aarch64; done
 else
   exec "$@"
 fi
