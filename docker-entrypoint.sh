357e181dcf8f2b171ce5b3e10dd29dea5adde3c3
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2021-03-09 19:09:13 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
-#!/bin/bash -le
+#!/bin/bash -l
 
-USER_ID=${LOCAL_UID:-9001}
-GROUP_ID=${LOCAL_GID:-9001}
+USER_ID=${UID:-9001}
+GROUP_ID=${GID:-9001}
 
-echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
-useradd -u $USER_ID -o -m user
+useradd -u $USER_ID -o -m user -s /bin/bash
 groupmod -g $GROUP_ID user
-export HOME=/app
 
 if [ "$#" -eq 0 ]; then
-  for ver in `ls ~/.pyenv/versions`; do
-      exec /usr/sbin/gosu user pyenv local $ver
-      exec /usr/sbin/gosu user python setup.py bdist_wheel -p aarch64
+  echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+  export HOME=/home/user
+  for ver in `ls /usr/pyenv/versions`; do
+      /usr/sbin/gosu user pyenv local $ver
+      /usr/sbin/gosu user python setup.py bdist_wheel -p aarch64
   done
 else
-  exec /usr/sbin/gosu user "$@"
+  exec "$@"
 fi
