84b655af63c94e94f18c32580b7912f6c604e314
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2021-03-09 12:52:19 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,18 @@
-#!/bin/sh -e
+#!/bin/bash -le
+
+USER_ID=${LOCAL_UID:-9001}
+GROUP_ID=${LOCAL_GID:-9001}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $USER_ID -o -m user
+groupmod -g $GROUP_ID user
+export HOME=/app
+
 if [ "$#" -eq 0 ]; then
-  find tests  -type d -name '__pycache__' -print0 | xargs -0 rm -rf {}
-  pyenv rehash
-  for ver in `ls ~/.pyenv/versions`; do pyenv local $ver; python setup.py bdist_wheel -p aarch64; done
+  for ver in `ls ~/.pyenv/versions`; do
+      exec /usr/sbin/gosu user pyenv local $ver
+      exec /usr/sbin/gosu user python setup.py bdist_wheel -p aarch64
+  done
 else
-  exec "$@"
+  exec /usr/sbin/gosu user "$@"
 fi
