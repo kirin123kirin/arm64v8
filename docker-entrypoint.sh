c38a1ad2a8a3f21f6a02d17547eb41cbb5fcd211
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2021-03-09 22:59:52 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 USER_ID=${UID:-9001}
 GROUP_ID=${GID:-9001}
+PLAT_NAME=${PLAT_NAME:manylinux2014_aarch64}
 
 useradd -u $USER_ID -o -m user -s /bin/bash
 groupmod -g $GROUP_ID user
@@ -11,7 +12,7 @@ if [ "$#" -eq 0 ]; then
   export HOME=/home/user
   for ver in `ls /usr/pyenv/versions`; do
       /usr/sbin/gosu user pyenv local $ver
-      /usr/sbin/gosu user python setup.py bdist_wheel -p aarch64
+      /usr/sbin/gosu user python setup.py bdist_wheel -p $PLAT_NAME
   done
 else
   exec "$@"
